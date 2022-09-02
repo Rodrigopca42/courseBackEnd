e2d73951f4e9559f831284b8364d9189274bc83e
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-09-02 20:08:42 -0300

    Update Product.java

--- estudo-back-end/src/main/java/br/com/bennytech/estudobackend/model/Product.java
+++ estudo-back-end/src/main/java/br/com/bennytech/estudobackend/model/Product.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class Product {
     
     @Id  //vai tranformar essa coluna em primary key
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
     private Integer id;
     private String name;
     private Integer quantit;
